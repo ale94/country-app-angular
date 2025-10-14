24f0f15061ccc63b420c18ecec161e87e9383258
ale94 <alejandrorua111@gmail.com>
2025-10-13 23:49:16 -0300

    feat: add by capital component

---
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-page',
+  imports: [],
+  templateUrl: './by-capital-page.html'
+})
+export class ByCapitalPage {
+
+  onSearch(value: string) {
+    console.log({ value });
+  }
+}
