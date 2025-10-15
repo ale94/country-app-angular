e95e90f61c491f7aa8cfb910ccefea51acb18b6a
ale94 <alejandrorua111@gmail.com>
2025-10-14 22:36:03 -0300

    feat: add by region component

---
+++ src/app/country/pages/by-region-page/by-region-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { List } from "../../components/list/list";
+
+@Component({
+  selector: 'app-by-region-page',
+  imports: [List],
+  templateUrl: './by-region-page.html'
+})
+export class ByRegionPage {
+
+}
