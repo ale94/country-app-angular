0195cc2092945a50038c874b68d47e1df6610ff8
ale94 <alejandrorua111@gmail.com>
2025-10-14 22:36:33 -0300

    feat: add by capital page component

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchInput } from '../../components/search-input/search-input';
+import { List } from "../../components/list/list";
 
 @Component({
   selector: 'app-by-capital-page',
-  imports: [],
+  imports: [SearchInput, List],
   templateUrl: './by-capital-page.html'
 })
 export class ByCapitalPage {
