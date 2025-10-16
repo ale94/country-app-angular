2f94cbe95758e6099969b4003302b3825aa71483
ale94 <alejandrorua111@gmail.com>
2025-10-15 23:30:12 -0300

    refactor: refactor components

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SearchInput } from '../../components/search-input/search-input';
 import { List } from "../../components/list/list";
+import { CountryService } from '../../services/country';
+import { RESTCountry } from '../../interfaces/rest-countries';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,7 +12,18 @@ import { List } from "../../components/list/list";
 })
 export class ByCapitalPage {
 
-  onSearch(value: string) {
-    console.log({ value });
+  countryService = inject(CountryService);
+  isLoading = signal(false);
+  isError = signal<string | null>(null);
+  countries = signal<Country[]>([]);
+
+  onSearch(query: string) {
+    if (this.isLoading()) return;
+    this.isLoading.set(true);
+    this.isError.set(null);
+    this.countryService.searchByCapital(query).subscribe(countries => {
+      this.isLoading.set(false);
+      this.countries.set(countries);
+    });
   }
 }
