791f7760fe76c27e1292832cf534d86447846d0d
ale94 <alejandrorua111@gmail.com>
2025-10-20 22:49:37 -0300

    feat: add error handling in the capital page component

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -2,8 +2,8 @@ import { Component, inject, signal } from '@angular/core';
 import { SearchInput } from '../../components/search-input/search-input';
 import { List } from "../../components/list/list";
 import { CountryService } from '../../services/country';
-import { RESTCountry } from '../../interfaces/rest-countries';
-import { Country } from '../../interfaces/country';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -13,17 +13,34 @@ import { Country } from '../../interfaces/country';
 export class ByCapitalPage {
 
   countryService = inject(CountryService);
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
+  query = signal("");
 
-  onSearch(query: string) {
-    if (this.isLoading()) return;
-    this.isLoading.set(true);
-    this.isError.set(null);
-    this.countryService.searchByCapital(query).subscribe(countries => {
-      this.isLoading.set(false);
-      this.countries.set(countries);
-    });
-  }
+  countryResource = rxResource({
+    params: () => ({ query: this.query() }),
+    stream: ({ params }) => {
+      if (!params.query) return of([]);
+      return this.countryService.searchByCapital(params.query)
+    }
+  })
+
+
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
+  // countries = signal<Country[]>([]);
+
+  // onSearch(query: string) {
+  //   if (this.isLoading()) return;
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
+  //   this.countryService.searchByCapital(query).subscribe({
+  //     next: (countries) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set(countries);
+  //     }, error: (err) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set([]);
+  //       this.isError.set(err)
+  //     },
+  //   });
+  // }
 }
