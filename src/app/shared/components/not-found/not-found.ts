8ebe1a98dba29121b168b0788507a9687bce69a4
ale94 <alejandrorua111@gmail.com>
2025-11-16 10:54:03 -0300

    feat: implement goBack method and update not-found component for navigation

--- src/app/shared/components/not-found/not-found.ts
+++ src/app/shared/components/not-found/not-found.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'not-found',
   imports: [],
   templateUrl: './not-found.html',
 })
-export class NotFound { }
+export class NotFound {
+
+  location = inject(Location);
+
+  goBack() {
+    this.location.back();
+  }
+}
