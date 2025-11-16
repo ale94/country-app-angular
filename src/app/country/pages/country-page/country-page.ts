2885f9f4c50701caf7b8b143ffb2fb601f6d4b3a
ale94 <alejandrorua111@gmail.com>
2025-11-16 10:54:17 -0300

    feat: implement country page with loading, error, and country info handling

--- src/app/country/pages/country-page/country-page.ts
+++ src/app/country/pages/country-page/country-page.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../services/country';
+import { NotFound } from '../../../shared/components/not-found/not-found';
+import { CountryInfo } from '../../components/country-info/country-info';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [NotFound, CountryInfo],
   templateUrl: './country-page.html'
 })
 export class CountryPage {
+
+  countryCode = inject(ActivatedRoute).snapshot.params['code'];
+  countryService = inject(CountryService);
+
+  countryResource = rxResource({
+    params: () => ({ code: this.countryCode }),
+    stream: ({ params }) => this.countryService.searchByCountryAlphaCode(params.code)
+  })
 
 }
