79db94502d4e17a56486243058d5a4f971c6dab3
ale94 <alejandrorua111@gmail.com>
2025-11-18 23:41:33 -0300

    feat: implement debounce functionality in search input component

--- src/app/country/components/search-input/search-input.ts
+++ src/app/country/components/search-input/search-input.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -9,5 +9,14 @@ export class SearchInput {
 
   placeholder = input<string>("Buscar");
   value = output<string>();
+  inputValue = signal<string>("");
+  debounceTime = input<number>(300);
 
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, this.debounceTime());
+    onCleanup(() => clearTimeout(timeout));
+  });
 }
