61eae623647d9690fb3871fd73647543bcdde373
ale94 <alejandrorua111@gmail.com>
2025-11-25 23:03:40 -0300

    feat: add searchByRegion method to CountryService for region-based queries

--- src/app/country/services/country.ts
+++ src/app/country/services/country.ts
@@ -4,6 +4,7 @@ import { RESTCountry } from '../interfaces/rest-countries';
 import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country-mapper';
 import { Country } from '../interfaces/country';
+import { Region } from '../interfaces/region.type';
 
 const API_URL = "https://restcountries.com/v3.1";
 
@@ -15,6 +16,7 @@ export class CountryService {
   private http = inject(HttpClient);
   private queryCacheCapital = new Map<string, Country[]>();
   private queryCacheCountry = new Map<string, Country[]>();
+  private queryCacheRegion = new Map<Region, Country[]>();
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase()
@@ -42,6 +44,20 @@ export class CountryService {
         tap(countries => this.queryCacheCountry.set(query, countries)),
         catchError(error => {
           return throwError(() => new Error(`No se pudo obtener paises con ese query ${query}`))
+        })
+      )
+  }
+
+  searchByRegion(region: Region): Observable<Country[]> {
+    if (this.queryCacheRegion.has(region)) {
+      return of(this.queryCacheRegion.get(region) ?? [])
+    }
+    return this.http.get<RESTCountry[]>(`${API_URL}/region/${region}`)
+      .pipe(
+        map(resp => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+        tap(countries => this.queryCacheRegion.set(region, countries)),
+        catchError(error => {
+          return throwError(() => new Error(`No se pudo obtener paises con ese query ${region}`))
         })
       )
   }
